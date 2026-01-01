59c4809b8ac198a329db4a0d1aadb7d590796395
aakashkdev <aakashvtu@gmail.com>
2026-01-01 23:52:22 +0530

    setup db connections

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_Name } from "./constants";
+import express from "express";
+
+const app = express();
+
+(async()=>{
+    try {
+       await mongoose.connect(`${process.env.MONGO_URI}/${DB_Name}`)
+       app.on("error", (error) => {
+           console.error("Error connecting to the server:", error);
+       });
+       app.listen(process.env.PORT, () => {
+           console.log(`Server is running on port ${process.env.PORT}`);
+       });
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}) ()
