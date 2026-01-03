08f9b5ea9a5549f0dceb1d14359d13193bcff66d
aakashkdev <aakashvtu@gmail.com>
2026-01-03 23:45:20 +0530

    connection setu

--- db/index.js
+++ db/index.js
@@ -3,15 +3,21 @@ import { DB_Name } from "../constants";
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI not found");
+    }
+
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGO_URI}/${DB_Name}`
     );
+
     console.log(
-      `\n MongoDB connected!! DB HOST: ${connectionInstance.connection.host} `
+      `MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`
     );
   } catch (error) {
-    console.log("MongoDB connection error:", error);
+    console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
 };
+
 export default connectDB;
